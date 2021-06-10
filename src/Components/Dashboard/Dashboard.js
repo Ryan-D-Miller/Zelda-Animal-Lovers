554252c71653f1eb2bd02a624a1bd49c2eec8829
Ryan Miller <ryandmiller3521@gmail.com>
2021-06-10 10:21:15 -0600

    Update Card data to pass indivdual props instead of all of the data

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,11 +1,21 @@
 import './Dashboard.css';
 import Card from '../Card/Card';
+import PropTypes from 'prop-types';
 
 function Dashboard({data, updateFound, title}) {
 
     const displayCards = data.map(data => {
         return (
-            <Card data={data} key={data.id} updateFound={updateFound}/>
+            <Card 
+                image={data.image} 
+                name={data.name} 
+                description={data.description} 
+                id={data.id} 
+                common_locations={data.common_locations}
+                isFound={data.isFound}
+                key={data.id} 
+                updateFound={updateFound}
+            />
         )
     });
 
@@ -17,6 +27,12 @@ function Dashboard({data, updateFound, title}) {
             </main>
         </section>
     )
+}
+
+Dashboard.prototypes = {
+    title: PropTypes.number,
+    data: PropTypes.object,
+    updateFound: PropTypes.func
 }
 
 export default Dashboard;
